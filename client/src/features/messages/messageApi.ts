5792f537675324864b6e2312f0b4e223e4d6bdf8
Fuyad Hasan Fahim <fuyadhasanfahim0@gmail.com>
2024-10-23 10:03:12 +0600

    store

--- client/src/features/messages/messageApi.ts
+++ client/src/features/messages/messageApi.ts
@@ -1,43 +1,22 @@
 import { apiSlice } from '../api/apiSlice'
-import { addMessage, setMessages } from '@/features/messages/messageSlice'
-import IMessage from '@/types/messageInterface'
 
 export const messageApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
-        getMessage: builder.query<IMessage[], string>({
+        getMessage: builder.query({
             query: (conversationId) => ({
                 url: `/messages/get-message/${conversationId}`,
                 method: 'GET',
             }),
             providesTags: ['Messages', 'Conversations'],
             keepUnusedDataFor: 0,
-            async onQueryStarted(
-                _conversationId,
-                { dispatch, queryFulfilled },
-            ) {
-                try {
-                    const { data } = await queryFulfilled
-                    dispatch(setMessages(data))
-                } catch (error) {
-                    console.error('Error fetching messages:', error)
-                }
-            },
         }),
-        setMessage: builder.mutation<IMessage, Partial<IMessage>>({
+        setMessage: builder.mutation({
             query: (data) => ({
                 url: '/messages/set-message',
                 method: 'POST',
                 body: data,
             }),
             invalidatesTags: ['Messages'],
-            async onQueryStarted(_message, { dispatch, queryFulfilled }) {
-                try {
-                    const { data } = await queryFulfilled
-                    dispatch(addMessage(data))
-                } catch (error) {
-                    console.error('Error adding message:', error)
-                }
-            },
         }),
     }),
 })
